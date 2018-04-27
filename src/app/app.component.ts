b6ef8f076f6247ad20c5743b04cda56d92374281
Andre Merchant <andremerchant@gmail.com>
2018-04-27 14:11:14 -0500

    funcionalidad añadida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'encuesta';
+  encuestaName:string = "Elige tu titulo";
+
+  campos: any = [ ]
+
+  contador: any = {
+      texto:0, casilla:0, opciones:0
+  }
+
+  addTexto(){
+    // this.encuestaName = "Texto";
+    var r_texto = prompt("¿Cuál es el título de tu campo?");
+    if (r_texto) {
+      this.campos.push({tipo: "texto", texto:r_texto});
+      this.contador.texto++;
+    }
+  }
+  addCasillas(){
+    var r_casillas = prompt("¿Cuál es el título de tu campo?");
+    if (r_casillas) {
+      this.campos.push({tipo: "casilla", texto:r_casillas});
+      this.contador.casilla++;
+    }
+  }
+  addDesplegable(){
+    var r_dropdown = prompt("¿Cuál es el título de tu campo?");
+    if (r_dropdown) {
+      this.campos.push({tipo: "opciones", texto:r_dropdown});
+      this.contador.opciones++;
+    }
+  }
 }
